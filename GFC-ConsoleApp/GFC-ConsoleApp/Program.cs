a3a4fa07fec00d76dcf6b52fea8e5117276e2942
Mahesh bisht <mahesh.singh3112@gmail.com>
2019-05-23 21:15:29 +0530

    added Project

---
+++ GFC-ConsoleApp/GFC-ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GFC_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
